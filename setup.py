0b0d1dc2018e0ee3507c07b02c0d065d92c3bfc6
Iuzu <zall3252@jcsnc.org>
2023-07-26 13:54:21 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='py-capbypass',
+    version='0.0.1',
+    description='A bare-bones library for capbypass.com.',
+    author='iuzu',
+    author_email='none',
+    packages=['py_capbypass'],
+    install_requires=['requests'],  # Add any required dependencies here
+)
